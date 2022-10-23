71a1a2973a6fe8d1ee4edbe116c723ee31a56e87
Alexander Dreweke <alexander@dreweke.net>
2022-10-23 23:20:09 +0200

    fix formatting

--- iskra.sql
+++ iskra.sql
@@ -1,9 +1,9 @@
 CREATE TABLE IF NOT EXISTS iskra (
-	time		        TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
+    time                TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP(0),
     id                  VARCHAR   NOT NULL,
     current_consumption REAL,
     total_consumption   REAL,
     total_supply        REAL,
-	UNIQUE		        (time, id)
+    UNIQUE              (time, id)
 );
 
